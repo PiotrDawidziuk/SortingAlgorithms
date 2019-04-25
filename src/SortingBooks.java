f123cd5e53c47edeaa971eab8c3f3c1c54e98fe3
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-25 09:25:23 +0200

    array added

--- src/SortingBooks.java
+++ src/SortingBooks.java
@@ -2,12 +2,19 @@ public class SortingBooks {
 
     public static void main(String[] args) {
 
-        Book book1= new Book("Potop","Sienkiewicz",30);
-        Book book2= new Book("Ogniem i Mieczem","Sienkiewicz",20);
-        Book book3= new Book("Makbet","Szekspir",10);
-        Book book4= new Book("Pan Tadeusz","Mickiewicz",20);
-        Book book5= new Book("Krzyżacy","Sienkiewicz",30);
+        Book book1= new Book("A Book","An Author",30);
+        Book book2= new Book("C Book","C. Author",20);
+        Book book3= new Book("D Book","D. Writer",10);
+        Book book4= new Book("B Book","B. Poet",20);
+        Book book5= new Book("E Book","Ellen Author",30);
         System.out.println(book1.toString());
+
+        int comp1to2 = book1.getTitle().compareTo(book2.getTitle());
+        System.out.println(comp1to2);
+
+        Book[] books = {book1,book2,book3,book4,book5};
+
+        System.out.println();
 
     }
 
