edb78c7d50d5139b1ccc2a43ff2a85956e56aed5
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-25 09:03:02 +0200

    Book class

---
+++ src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+
+    String title;
+    String author;
+    int price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+
+        return title+" "+author;
+    }
+}
