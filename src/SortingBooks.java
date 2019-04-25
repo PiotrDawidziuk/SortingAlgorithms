791079aa8a7e62b5c9a666a1ede4d90a16f95854
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-25 09:34:26 +0200

    books sorted

--- src/SortingBooks.java
+++ src/SortingBooks.java
@@ -1,5 +1,20 @@
 public class SortingBooks {
 
+    void sortingBooks(Book arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j].getTitle().compareTo(arr[j+1].getTitle())>0)
+                {
+                    // swap arr[j+1] and arr[i]
+                    Book temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+
+
     public static void main(String[] args) {
 
         Book book1= new Book("A Book","An Author",30);
@@ -14,8 +29,16 @@ public class SortingBooks {
 
         Book[] books = {book1,book2,book3,book4,book5};
 
-        System.out.println();
+        for (Book book : books){
+            System.out.println(book.toString());
+        }
 
+        SortingBooks sortingBooks = new SortingBooks();
+        sortingBooks.sortingBooks(books);
+
+        for (Book book : books){
+            System.out.println(book.toString());
+        }
     }
 
 }
